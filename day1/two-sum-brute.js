0df7e6269c924e50ac5c0a2152e2f7379b867bf3
prateek saini <pratiiksaini@gmail.com>
2023-08-22 00:35:40 +0530

    adding two sum problem

---
+++ day1/two-sum-brute.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let len = nums.length;
+    let final = [];
+    for (let i = 0; i < len; i++) {
+        for (let j = i + 1; j < len; j++) {
+            if (nums[i] + nums[j] == target) {
+                final.push(i, j);
+                return final;
+            }
+        }
+    }
+};
---
+++ day1/two-sum-optimal.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let numHashTable = {};
+    for( let i in nums){
+        let comp = target - nums[i];
+        if(numHashTable[comp]>-1){
+            return [numHashTable[comp], i]
+        }
+        numHashTable[nums[i]] = i
+    }
+};
