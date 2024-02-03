3779a48b868a1e1a0192c8ef76ccad7e1a76b8c0
Prateek Saini <pratiiksaini@gmail.com>
2024-02-03 14:05:48 +0530

    adding split  array

---
+++ day3/split-k-array.js
@@ -0,0 +1,25 @@
+// Divide the array into K subarray of size ‘k’.
+
+// Input: [1, 3, 2, 3, 2, 6, -1, 4, 1,12, 8, 2, 14], k=3
+// Output: [[1, 3, 2], [3, 2, 6], [-1, 4, 1], [12, 8, 2], [14]];
+
+let ar = [1, 3, 2, 3, 2, 6, -1, 4, 1, 12, 8, 2, 14], k = 5;
+
+function splitArr(ar, k) {
+    let len = ar.length;
+    let finalArr = [];
+    let tmpAr = [];
+    for (let windowEnd = 0; windowEnd < len; windowEnd++) {
+        tmpAr.push(ar[windowEnd]);
+        if (windowEnd % k === k - 1) {
+            finalArr.push(tmpAr);
+            tmpAr = [];
+        }
+        if (windowEnd === len - 1 && tmpAr.length) {
+            finalArr.push(tmpAr);
+        }
+    }
+    console.log(finalArr);
+}
+
+splitArr(ar, k);
